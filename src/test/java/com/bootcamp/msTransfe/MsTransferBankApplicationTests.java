940786dd871285fcd771c0ed0929f5716743b746
rosasjose96 <34173505+rosasjose96@users.noreply.github.com>
2021-08-16 07:42:40 -0500

    initial commit

---
+++ src/test/java/com/bootcamp/msTransfe/MsTransferBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.msTransfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTransferBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
